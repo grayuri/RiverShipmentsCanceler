e467702ddf5f2df7e120289331432a9b8615d672
grayuri <bv6778145@gmail.com>
2024-09-26 12:15:25 -0300

    River Script fixed

--- riverScript.js
+++ riverScript.js
@@ -1,26 +1,37 @@
 (async () => {
   const utilsSrc = browser.runtime.getURL("./utils.js");
-  const { createSubmitButton } = await import(utilsSrc)
+  const { createSubmitButton, createElementWithText } = await import(utilsSrc)
 
   const { shipments } = await browser.storage.sync.get("shipments")
   const notPermitedShipments = shipments.filter(shipment => shipment.status === "NOT_PERMITED")
-  let allShipmentsWasInserted = false
 
   const riverContainer = document.querySelector("header")
   const pasteShipmentsButton = createSubmitButton("Paste Shipments")
+  const message = createElementWithText("p", "")
 
   riverContainer.appendChild(pasteShipmentsButton)
-
+  riverContainer.appendChild(message)
+  
   function insertValuesIntoInputs() {
-    const formControlInputs = document.querySelectorAll("input")
-
+    let totalOfShipmentsInserted = 0
+    const formControlInputs = document.querySelectorAll(".form-control")
+    
     formControlInputs.forEach((input, index) => {
-      if (!index === notPermitedShipments.length) input.value = notPermitedShipments[index]
-      else allShipmentsWasInserted = true
+      if (totalOfShipmentsInserted === notPermitedShipments.length) {
+        message.innerText = "All shipments were inserted."
+      }
+      else {
+        if (!(input.value === notPermitedShipments[index].id)) input.value = notPermitedShipments[index].id
+        
+        totalOfShipmentsInserted++
+
+        let shipmentsRemaining = notPermitedShipments.length - totalOfShipmentsInserted
+        message.innerText = "There's " + shipmentsRemaining + " shipments remaining."
+      }
     })
   }
 
   pasteShipmentsButton.addEventListener("click", () => {
-    if (!allShipmentsWasInserted) setInterval(insertValuesIntoInputs, 2000)
+    insertValuesIntoInputs()
   })
 })()
